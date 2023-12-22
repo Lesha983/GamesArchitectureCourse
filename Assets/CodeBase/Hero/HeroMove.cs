438aff488d1b59df43d6f031afdc7f67fe02c22d
Alexei <lzelenkov@mail.ru>
2023-12-22 22:43:41 +0300

    Add Input

---
+++ Assets/CodeBase/Hero/HeroMove.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using CodeBase.CameraLogic;
+using CodeBase.Infrastructure;
+using CodeBase.Services;
+using UnityEngine;
+
+using SF = UnityEngine.SerializeField;
+
+namespace CodeBase.Hero
+{
+	public class HeroMove : MonoBehaviour
+	{
+		[SF] private CharacterController characterController;
+		[SF] private float movmentSpeed;
+		private IInputService _inputService;
+		private Camera _camera;
+
+		private void Awake()
+		{
+			_inputService = Game.inputService;
+		}
+
+		private void Start()
+		{
+			_camera = Camera.main;
+			CameraFollow();
+		}
+
+		private void Update()
+		{
+			var movmentVector = Vector3.zero;
+			if (_inputService.Axis.sqrMagnitude > Constants.Epsilon)
+			{
+				movmentVector = _camera.transform.TransformDirection(_inputService.Axis);
+				movmentVector.y = 0;
+				movmentVector.Normalize();
+
+				transform.forward = movmentVector;
+			}
+
+			movmentVector += Physics.gravity;
+			characterController.Move(movmentSpeed * movmentVector * Time.deltaTime);
+		}
+
+		private void CameraFollow() =>
+			_camera.GetComponent<CameraFollow>().Follow(transform);
+	}
+}
---
+++ Assets/CodeBase/Infrastructure/Game.cs
@@ -0,0 +1,23 @@
+﻿using CodeBase.Services;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+	public class Game
+	{
+		public static IInputService inputService;
+
+		public Game()
+		{
+			RegistorInputService();
+		}
+
+		private void RegistorInputService()
+		{
+			if (Application.isEditor)
+				inputService = new StandaloneInputService();
+			else
+				inputService = new MobileInputService();
+		}
+	}
+}
